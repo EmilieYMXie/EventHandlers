6019b21d3bc7a7e33376529f07b991752672c1a4
EmilieYMXie <ex2112@columbia.edu>
2015-04-08 19:35:03 -0400

    Turn scripts into string

--- scripts.js
+++ scripts.js
@@ -1,13 +1,17 @@
 for (var i=0; i<guardianHeroinData.length; i++) {
 	var rowData = guardianHeroinData[i];
 	
-	var template = '<div id="dataContainer">'+
-		'<div class="quoteContainer">'
+	var template = 
+		'<div class="quoteContainer">'+
 			'<div class="mainQuote">'+rowData.mainQuote+'</div>'+
 			'<div class="wholeQuote">'+rowData.wholeQuote+'</div>'+
 			'<div class="source">'+rowData.source+'</div>'+
 			'<div class="yearsClean">'+rowData.yearsClean+'</div>'+
 			'<div class="location">'+rowData.location+'</div>'+
-		'</div>'+
+		'</div>';
+		
+		$('#dataContainer').append(template);
 }
+
+
 
