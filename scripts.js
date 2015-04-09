a57d3f6f5c94ed03617f8bc275ffcc422c94b35c
EmilieYMXie <ex2112@columbia.edu>
2015-04-08 20:45:54 -0400

    Update scripts and styles file

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 for (var i=0; i<guardianHeroinData.length; i++) {
 	var rowData = guardianHeroinData[i];
 	
-	var template = '<div class="quoteContainer">'+
+	var $template = $('<div class="quoteContainer">'+
 						'<div class="mainQuote">'+rowData.mainQuote+'</div>'+
 						'<div class="readMore">Read More</div>'+
 						'<div class="more.Info">'+
@@ -10,22 +10,22 @@ for (var i=0; i<guardianHeroinData.length; i++) {
 							'<div class="yearsClean">'+rowData.yearsClean+'</div>'+
 							'<div class="location">'+rowData.location+'</div>'+
 						'</div>'+
-					'</div>';
+					'</div>');
 		
-		$(template).find(".readMore").on("click", toggleContent);
+		$template.find(".readMore").on("click", toggleContent);
 		
+		$('#dataContainer').append($template);
+}
+
 		function toggleContent(e) {
-			$quoteContainer = $(this).parent();
+			$quoteContainer = $(e.target).parent();
 			
-			if ($quoteContainer.hasClass("expanded")) { //quoteContainer has class. expanded (you can see content), so remove content
+			if ($quoteContainer.hasClass("expanded")) { //I'm checking to see if a class is there, quoteContainer has class. expanded (you can see content), so remove content
 				$quoteContainer.removeClass("expanded");
 			} else { // quoteContainer does not have class expanded (you can't see content), so show content
 				$quoteContainer.addClass("expanded");
 			}
 		}
 		
-		$('#dataContainer').append(template);
-}
-
 
 
